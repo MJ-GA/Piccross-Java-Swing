c9128ad3014c50acd9dfa187bf46ea5aec7ed160
Mohamed Jouini <joui0005@algonquinlive.com>
2023-03-04 20:53:12 -0500

    .

--- Piccross__1/Piccross/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Piccross","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Piccross","l":"Piccross()","u":"%3Cinit%3E()"}];updateSearchResults();
--- Piccross__1/bin/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"GameController"},{"p":"<Unnamed>","l":"Piccross"}];updateSearchResults();
